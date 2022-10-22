bd098bfb80825b2bca3edd9dcd86ec7f75dce27f
fenghai.ning <fenghai.ning@megatronix.co>
2022-10-22 19:49:03 +0800

    [ADD] add learn.md

--- add.c
+++ add.c
@@ -1,11 +1,28 @@
-int add(int a, int b) {
-  int c = a + b;
+#include <stdint.h>
+
+uint64_t add(uint64_t a, uint64_t b) {
+  uint64_t c = a + b;
   return c;
 }
 
 int main() {
-  int a = 10;
-  int b = 9;
-  int c = add(a, b);
+  uint64_t a = 0x12340000;
+  uint64_t b = 0x0000abcd;
+  uint64_t c = add(a, b);
+
   return 0;
 }
+
+/*
+
+0x7fffffffdc10: 0x0000a000
+0x7fffffffdc0f: 0x00a00012
+0x7fffffffdc0e: 0xa0001234
+0x7fffffffdc0d: 0x00123456
+0x7fffffffdc0c: 0x12345678
+0x7fffffffdc0b: 0x34567887
+0x7fffffffdc0a: 0x56788765
+0x7fffffffdc09: 0x78876543
+0x7fffffffdc08: 0x87654321
+
+*/
--- ass/src/main.c
+++ ass/src/main.c
@@ -1,10 +1,38 @@
+#include <stdint.h>
+
+#include "disk/elf.h"
 #include "cpu/register.h"
+#include "cpu/mmu.h"
 #include "memory/instruction.h"
+#include "memory/dram.h"
 
 int main(int argc, char *argv[]) {
 
     init_handler_table();
     
+    // init register
+    reg.rax = 0x12340000;
+    reg.rbx = 0x0;
+    reg.rcx = 0x8000660;
+    reg.rdx = 0xabcd;
+    reg.rsi = 0x7ffffffee2f8;
+    reg.rdi = 0x1;
+    reg.rbp = 0x7ffffffee210;
+    reg.rsp = 0x7ffffffee1f0;
     
+    reg.rip = *(uint64_t *)&(program[11]);
+
+    // init memory
+    mm[va2pa(0x7ffffffee210)] = 0x08000660;         // rbp
+    mm[va2pa(0x7ffffffee208)] = 0x0;
+    mm[va2pa(0x7ffffffee200)] = 0xabcd;
+    mm[va2pa(0x7ffffffee1f8)] = 0x12340000;
+    mm[va2pa(0x7ffffffee1f0)] = 0x08000660;         // rsp
+
+    // run inst
+    
+
+    // match out
+
     return 0;
 }
